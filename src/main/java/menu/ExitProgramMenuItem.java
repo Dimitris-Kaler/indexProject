5fb56a85cc7296b8747305d688e569d768ddf053
kalerantesd <kalerantesd@KALERANTLAP-W10.interamerican.gr>
2022-08-31 12:07:46 +0300

    cleaning the code fixing execute method at ExitProgramMenuItem

--- src/main/java/menu/ExitProgramMenuItem.java
+++ src/main/java/menu/ExitProgramMenuItem.java
@@ -1,11 +1,15 @@
 package menu;
 
+import java.util.Scanner;
+
+import IndexProject.Index;
+
 public class ExitProgramMenuItem extends MenuItem {
 	public ExitProgramMenuItem() {
 		super("5","Exit The Program");
 	}
 	@Override
-	public void execute() {
+	public void execute(Index index,Scanner scanner) {
 		System.out.println("Bye Bye see you soon!!");
 		System.exit(0);
 	}
